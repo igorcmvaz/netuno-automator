aebd250f37aa25bdc7f2eecfd0e1b1ddee1e923f
Matheus Bruhns Bastos <matbbastos@gmail.com>
2025-03-12 14:02:42 -0300

    feat: add validator for command line arguments and corresponding errors

---
+++ globals/errors.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+class InvalidNetunoExecutableError(Exception):
+    def __init__(self, path_to_netuno: Path, *args):
+        message = f"No file at '{path_to_netuno.resolve()}'"
+        super().__init__(message, *args)
+
+
+class InvalidSourceDirectoryError(Exception):
+    def __init__(self, source_directory: Path, *args):
+        message = f"Provided path '{source_directory.resolve()}' is not a directory"
+        super().__init__(message, *args)
+
+
+class MissingInputDataError(Exception):
+    def __init__(self, source_directory: Path, *args):
+        message = f"Provided path '{source_directory.resolve()}' has no CSV files"
+        super().__init__(message, *args)
