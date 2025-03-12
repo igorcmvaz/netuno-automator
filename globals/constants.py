153925903cfa76f3756936fa1298c494eb7a60f5
Matheus Bruhns Bastos <matbbastos@gmail.com>
2025-03-12 13:54:46 -0300

    feat: create configuration constants

---
+++ globals/constants.py
@@ -0,0 +1,29 @@
+COEFFICIENT_OF_LOSS_MIN = 0.1
+COEFFICIENT_OF_LOSS_MAX = 1.0
+RAINFALL_SUBSTITUTION_PERCENT_MIN = 10
+RAINFALL_SUBSTITUTION_PERCENT_MAX = 100
+
+NETUNO_STARTUP_WAIT_TIME = 1
+PAUSE = 0.8
+
+INITIAL_DATES = {
+    "Histórico": "1980-01-01",
+    "SSP245": "2015-01-01",
+    "SSP585": "2015-01-01"
+}
+
+SIMULATION_PARAMETERS = {
+    "initial_run_off_disposal": 2,
+    "catchment_area": 50,
+    "daily_water_demand": 603,
+    "residents": 1,
+    "rainwater_replacement_percentage": 40,
+    "coefficient_of_loss": 0.8,
+    "inferior_tank_capacity": 150
+}
+
+SIMULATION_OUTPUT_ATTRIBUTES = {
+    "encoding": "WINDOWS-1252",
+    "delimiter": ";",
+    "skip_n_rows": 22
+}
