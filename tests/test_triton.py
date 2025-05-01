0d00786317d56947b55bfdec6b4d48f9d827c3ce
Matheus Bruhns Bastos <matbbastos@gmail.com>
2025-05-01 12:53:50 -0300

    test: add tests for new function and changes to main()

--- tests/test_triton.py
+++ tests/test_triton.py
@@ -1,12 +1,12 @@
 import logging
 import unittest
 from pathlib import Path
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
 from agents.validators import CommandLineArgsValidator
 from globals.errors import CustomTimeoutError
 from tests.test_parsers import PATH_TO_SIMULATION_RESULT
-from triton import logger, main, run_netuno, setup_logger, sleep_until
+from triton import logger, main, restart_netuno, run_netuno, setup_logger, sleep_until
 
 MOCK_STRINGS = {
     "popen": "subprocess.Popen",
@@ -19,7 +19,7 @@ MOCK_STRINGS = {
 }
 
 
-class TestHelperFunctions(unittest.TestCase):
+class TestNetunoFunctions(unittest.TestCase):
 
     def test_run_netuno(self):
         path = Path(__file__).parent / "netuno.exe"
@@ -37,6 +37,40 @@ class TestHelperFunctions(unittest.TestCase):
             self.assertIn(EXPECTED_LOG_MESSAGE, log_context.output[0])
             sleep_mock.assert_called_once_with(5)
         self.assertEqual(result, new_process)
+
+    def test_restart_netuno(self):
+        path = Path(__file__).parent / "netuno.exe"
+        first_process = MagicMock()
+        first_process.pid = 1234
+        second_process = MagicMock()
+        second_process.pid = 5678
+        processes = {"netuno": first_process}
+
+        EXPECTED_LOG_MESSAGES = [
+            "Successfully spawned new process #1234 with Netuno 4",
+            "Terminating Netuno process #1234",
+            "Successfully spawned new process #5678 with Netuno 4",
+        ]
+        with (
+                patch(MOCK_STRINGS["popen"]) as popen_mock,
+                patch(MOCK_STRINGS["sleep"]) as sleep_mock,
+                self.assertLogs(logger, level=logging.DEBUG) as log_context):
+            popen_mock.side_effect = [first_process, second_process]
+            run_netuno(path, wait_after_start=5)
+            popen_mock.assert_called_once_with(args=(path,))
+
+            self.assertIn(EXPECTED_LOG_MESSAGES[0], log_context.output[0])
+            sleep_mock.assert_called_once_with(5)
+
+            restart_netuno(processes, wait_after_start=5)
+            popen_mock.assert_has_calls([call(args=(path,))])
+            self.assertIn(EXPECTED_LOG_MESSAGES[1], log_context.output[1])
+            self.assertIn(EXPECTED_LOG_MESSAGES[2], log_context.output[2])
+
+        self.assertEqual(processes.get("netuno"), second_process)
+
+
+class TestHelperFunctions(unittest.TestCase):
 
     def test_setup_logger_verbose(self):
         test_logger = logging.getLogger("test_logger")
@@ -94,7 +128,7 @@ class TestHelperFunctions(unittest.TestCase):
 
 class TestMainFunction(unittest.TestCase):
 
-    def test_main(self):
+    def test_main_no_restart(self):
         SAMPLE_FILE_NAME = "test-consolidated.csv"
         SAMPLE_RESULTS_FILE = Path(__file__).parent.parent / SAMPLE_FILE_NAME
         args = CommandLineArgsValidator()
@@ -104,20 +138,62 @@ class TestMainFunction(unittest.TestCase):
         args.verbose = True
         args.clean = True
         args.save_every = 2
+        args.restart_every = 15
+        args.wait = 0
+        netuno_process = MagicMock()
+        processes = {"netuno": netuno_process}
 
         with (
                 patch(MOCK_STRINGS["run_first"]) as mock_first_simulation,
                 patch(MOCK_STRINGS["run_simulation"]) as mock_run_simulation,
                 patch(MOCK_STRINGS["base_file_name"]) as mock_base_file_name,
                 patch(MOCK_STRINGS["path_unlink"]) as mock_unlink,
+                patch(MOCK_STRINGS["sleep"]),
                 patch(MOCK_STRINGS["sleep_until"])):
             mock_first_simulation.return_value = PATH_TO_SIMULATION_RESULT
             mock_run_simulation.return_value = PATH_TO_SIMULATION_RESULT
             mock_base_file_name.return_value = SAMPLE_FILE_NAME
-            main(args)
+            main(args, processes)
             mock_first_simulation.assert_called_once()
             mock_unlink.assert_called()
             self.assertEqual(mock_run_simulation.call_count, 4)
+        SAMPLE_RESULTS_FILE.unlink(missing_ok=True)
+
+    def test_main_with_restart(self):
+        SAMPLE_FILE_NAME = "test-consolidated.csv"
+        SAMPLE_RESULTS_FILE = Path(__file__).parent.parent / SAMPLE_FILE_NAME
+        args = CommandLineArgsValidator()
+        args.netuno_exe_path = Path(__file__).parent / "netuno.exe"
+        args.precipitation_dir_path = Path(__file__).parent.parent / "example"
+        file_count = len(list(args.precipitation_dir_path.iterdir()))
+        args.quiet = 0
+        args.verbose = True
+        args.clean = False
+        args.save_every = file_count
+        args.restart_every = file_count - 2
+        args.wait = 0
+
+        first_process = MagicMock()
+        first_process.pid = 1234
+        processes = {"netuno": first_process}
+
+        with (
+                patch(MOCK_STRINGS["run_first"]) as mock_first_simulation,
+                patch(MOCK_STRINGS["run_simulation"]) as mock_run_simulation,
+                patch(MOCK_STRINGS["base_file_name"]) as mock_base_file_name,
+                patch(MOCK_STRINGS["path_unlink"]) as mock_unlink,
+                patch(MOCK_STRINGS["popen"]) as popen_mock,
+                patch(MOCK_STRINGS["sleep"]),
+                patch(MOCK_STRINGS["sleep_until"])):
+            popen_mock.return_value = first_process
+            mock_first_simulation.return_value = PATH_TO_SIMULATION_RESULT
+            mock_run_simulation.return_value = PATH_TO_SIMULATION_RESULT
+            mock_base_file_name.return_value = SAMPLE_FILE_NAME
+            main(args, processes)
+            self.assertEqual(popen_mock.call_count, 1)
+            self.assertEqual(mock_first_simulation.call_count, 2)
+            self.assertEqual(mock_run_simulation.call_count, file_count - 2)
+            self.assertEqual(mock_unlink.call_count, 0)
         SAMPLE_RESULTS_FILE.unlink(missing_ok=True)
 
 
